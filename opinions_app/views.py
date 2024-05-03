95d57994d0e4893b34f0921d92e1164e0c099440
MarioRafaelBecerraDuenias <157363993+MarioRafaelBecerraDuenias@users.noreply.github.com>
2024-05-03 19:46:52 +0300

    Update views.py

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -3,40 +3,40 @@ from random import randrange
 from flask import abort, flash, redirect, render_template, url_for
 
 from . import app, db
-from .forms import OpinionForm
-from .models import Opinion
+from .forms import ReviewForm
+from .models import Review
 
 
 @app.route('/')
 def index_view():
-    quantity = Opinion.query.count()
+    quantity = Review.query.count()
     if not quantity:
         abort(404)
     offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
-    return render_template('opinion.html', opinion=opinion)
+    review = Review.query.offset(offset_value).first()
+    return render_template('review.html', review=review)
 
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
-    form = OpinionForm()
+def add_review_view():
+    form = ReviewForm()
     if form.validate_on_submit():
         text = form.text.data
-        if Opinion.query.filter_by(text=text).first() is not None:
+        if Review.query.filter_by(text=text).first() is not None:
             flash('This review already exists!')
-            return render_template('add_opinion.html', form=form)
-        opinion = Opinion(
+            return render_template('add_review.html', form=form)
+        review = Review(
             title=form.title.data,
             text=form.text.data,
             source=form.source.data
         )
-        db.session.add(opinion)
+        db.session.add(review)
         db.session.commit()
-        return redirect(url_for('opinion_view', id=opinion.id))
-    return render_template('add_opinion.html', form=form)
+        return redirect(url_for('review_view', id=review.id))
+    return render_template('add_review.html', form=form)
 
 
-@app.route('/opinions/<int:id>')
-def opinion_view(id):
-    opinion = Opinion.query.get_or_404(id)
-    return render_template('opinion.html', opinion=opinion)
+@app.route('/reviews/<int:id>')
+def review_view(id):
+    review = Review.query.get_or_404(id)
+    return render_template('review.html', review=review)
