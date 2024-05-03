1fd0216280d33858ade0bcf34de61ef216548d3d
MarioRafaelBecerraDuenias <157363993+MarioRafaelBecerraDuenias@users.noreply.github.com>
2024-05-03 19:44:53 +0300

    Update models.py

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from opinions_app import db
+from reviews_app import db
 
 
-class Opinion(db.Model):
+class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
